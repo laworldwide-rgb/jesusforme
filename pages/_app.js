782b2ac20b37cb69d155eee285de575377810646
laworldwide-rgb <laworldwide@gmail.com>
2026-03-17 16:04:32 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .catch((err) => console.log('SW registration failed:', err));
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#E83050" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Jesus FOR ME" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
