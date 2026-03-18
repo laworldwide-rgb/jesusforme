a340a5b87bdf8604907e71d6b1207cc5eabce3ca
laworldwide-rgb <laworldwide@gmail.com>
2026-03-18 16:15:45 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,3 @@
-import { useState } from "react";
-import ModeSelect from "../components/ModeSelect";
-import ParentDashboard from "../components/ParentDashboard";
-
 export default function Home() {
-  const [mode, setMode] = useState(null);
-
-  if (!mode) {
-    return <ModeSelect onSelect={setMode} />;
-  }
-
-  if (mode === "parent") {
-    return <ParentDashboard />;
-  }
-
-  return <div>Child mode coming next</div>;
+  return <div>SAFE TEST</div>;
 }
